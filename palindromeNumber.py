0b419ab3fee98236677b1ba37abb60696e61f556
Flahz <mik.lapierre07@gmail.com>
2024-05-20 14:57:13 -0400

    dawd

--- palindromeNumber.py
+++
@@ -1,8 +0,0 @@
-def isPalindrome(x):
-    x_string = str(x)
-    for i in range(len(x_string)):
-        if x_string[i] != x_string[len(x_string)-1-i]:
-            return False
-    return True
-
-print(isPalindrome(1234554321))
