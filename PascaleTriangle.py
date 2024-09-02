79f009186542ee2c2f5a37a41e75d0ff9ce2b203
Flahz <mik.lapierre07@gmail.com>
2024-09-02 00:18:11 -0400

    pascale triangle temrtiner manque lautre

--- PascaleTriangle.py
+++ PascaleTriangle.py
@@ -1,17 +1,21 @@
-def generate(numsRows):
-    
-    top_value = numsRows[-1]
-    if isinstance(numsRows, int):
-        return generate([[1]])
-    i=1
-    numsAdd = []
-    for i in range(len(top_value)):
-        if i == 1:
-            numsAdd[i] = 1
-        if i == len(top_value):
-            numsAdd[i] = 1
-        else:
-            numsAdd[i]= top_value[i] + top_value[i-1]
-    return generate(numsAdd)
+def generate(numRows):
+    list1 = [[1]]
+    n = 2
+    while n < numRows + 1:
+        list2 = []
+        for k in range(n):
+            if k == 0:
+                list2.append(1)
+            elif k == n-1:
+                list2.append(1)
+            else:
+                list2.append(list1[n-2][k-1] + list1[n-2][k])
+        list1.append(list2)
+        n += 1
+    return list1
 
 print(generate(5))
+    
+
+   
+
